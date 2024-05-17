1d6e1f9030a7647e1bd61185720b80d5fb9f4124
Leonard Yu <leonard.yu@teledyne.com>
2024-05-17 09:24:59 +0200

    Update ORAN DOS

--- oran_dos/c_plane_ecpri_dos_attack.py
+++ oran_dos/c_plane_ecpri_dos_attack.py
@@ -13,8 +13,11 @@ from headers import *
 CHASSIS_IP = "demo.xenanetworks.com"
 USERNAME = "oran_dos"
 PORT = "7/1"
+XPC_MODE = True
+DURATION = 10
+XPC_FILENAME = "ecpri_dos.xpc"
 
-async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
+async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str, xpc_mode: bool, duration: int):
 
     _mid = int(port_str.split("/")[0])
     _pid = int(port_str.split("/")[1])
@@ -24,11 +27,14 @@ async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
     print(f"Chassis:            {chassis_ip}")
     print(f"Username:           {username}")
     print(f"Port:               {port_str}")
+    print(f"XPC mode:           {xpc_mode}")
+    print(f"Test Duration:      {duration} sec")
     print(f"#####################################################################")
 
 
     # Establish connection to a Valkyrie tester using Python context manager
     # The connection will be automatically terminated when it is out of the block
+    print(f"Connect to chassis")
     async with testers.L23Tester(host=CHASSIS_IP, username=USERNAME, password="xena", port=22606, enable_logging=False) as tester:
 
         # Access module index 0 on the tester
@@ -41,51 +47,67 @@ async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
         port = module.ports.obtain(_pid)
 
         # Forcibly reserve the TX port and reset it.
+        print(f"Reserve port {port.kind.module_id}/{port.kind.port_id}")
         await mgmt.reserve_port(port)
         await mgmt.reset_port(port)
         await asyncio.sleep(2)
 
-        # Configure port from .xpc file
-        # await cli.port_config_from_file(port, "ecpri_dos.xpc")
+        if xpc_mode:
+            # Configure port from .xpc file
+            print(f"Load {XPC_FILENAME}")
+            await cli.port_config_from_file(port, XPC_FILENAME)
+        else:
+            print(f"Configure port and stream")
+            # Configure port using native python
+            await port.max_header_length.set(max_header_length=256) # Port's max header length = 128
+            stream = await port.streams.create() # create a stream on the port
+            
+            # Prepare packet header data
+            eth = Ethernet()
+            eth.dst_mac = "0080.1600.0000"
+            eth.src_mac = "0030.051d.1e27"
+            eth.ethertype = "AEFE"
 
-        # Configure port using native python
-        await port.max_header_length.set(max_header_length=256) # Port's max header length = 128
-        stream = await port.streams.create() # create a stream on the port
-        
-        # Prepare packet header data
-        eth = Ethernet()
-        eth.dst_mac = "0080.1600.0000"
-        eth.src_mac = "0030.051d.1e27"
-        eth.ethertype = "AEFE"
+            ecpri = eCPRIGeneralDataTransfer()
+            
+            await utils.apply(
+                stream.comment.set(comment="C-Plane eCPRI DoS Attack"), # stream description
+                stream.rate.fraction.set(stream_rate_ppm=100_000), # rate in ppm, 10%=100_000, 100%=1_000_000
+                stream.burst.burstiness.set(size=0, density=100), # non-burst, burst size = 0, burst density = 100%
+                stream.packet.header.protocol.set(segments=[
+                    enums.ProtocolOption.ETHERNET,
+                    enums.ProtocolOption.RAW_28]), # stream header, Ethernet | RAW28
+                stream.packet.header.data.set(hex_data=Hex(str(eth)+str(ecpri))),
+                stream.packet.length.set(length_type=enums.LengthType.FIXED, min_val=68, max_val=68),
+                stream.payload.content.set(payload_type=enums.PayloadType.PATTERN, hex_data=Hex("00")),
+                stream.tpld_id.set(test_payload_identifier=-1),
+                stream.insert_packets_checksum.set_on(),
+                stream.enable.set_on()
+            )
+            
+            await stream.packet.header.modifiers.configure(1) # create one modifier and configure
+            mod0 = stream.packet.header.modifiers.obtain(0) # access the created modifier
+            # configure the modifier
+            # place the modifier on header position 6
+            await mod0.specification.set(position=6, mask=Hex("FFFF0000"), action=enums.ModifierAction.RANDOM, repetition=1) 
+            await mod0.range.set(min_val=0, step=1, max_val=65535)
 
-        ecpri = eCPRIGeneralDataTransfer()
-        
-        await utils.apply(
-            stream.comment.set(comment="C-Plane eCPRI DoS Attack"), # stream description
-            stream.rate.fraction.set(stream_rate_ppm=100_000), # rate in ppm, 10%=100_000, 100%=1_000_000
-            stream.burst.burstiness.set(size=0, density=100), # non-burst, burst size = 0, burst density = 100%
-            stream.packet.header.protocol.set(segments=[
-                enums.ProtocolOption.ETHERNET,
-                enums.ProtocolOption.RAW_28]), # stream header, Ethernet | RAW28
-            stream.packet.header.data.set(hex_data=Hex(str(eth)+str(ecpri))),
-            stream.packet.length.set(length_type=enums.LengthType.FIXED, min_val=68, max_val=68),
-            stream.payload.content.set(payload_type=enums.PayloadType.PATTERN, hex_data=Hex("00")),
-            stream.tpld_id.set(test_payload_identifier=-1),
-            stream.insert_packets_checksum.set_on(),
-            stream.enable.set_on()
-        )
-        
-        await stream.packet.header.modifiers.configure(1) # create one modifier and configure
-        mod0 = stream.packet.header.modifiers.obtain(0) # access the created modifier
-        # configure the modifier
-        # place the modifier on header position 6
-        await mod0.specification.set(position=6, mask=Hex("FFFF0000"), action=enums.ModifierAction.RANDOM, repetition=1) 
-        await mod0.range.set(min_val=0, step=1, max_val=65535)
+        # start traffic
+        print(f"Start traffic")
+        await port.traffic.state.set_start()
+        # test duration
+        await asyncio.sleep(duration)
+        # stop traffic
+        print(f"Stop traffic")
+        await port.traffic.state.set_stop()
+        await mgmt.free_port(port)
+
+        print(f"Done")
 
 async def main():
     stop_event = asyncio.Event()
     try:
-        await s_plane_ptp_dos(CHASSIS_IP, PORT, USERNAME)
+        await s_plane_ptp_dos(CHASSIS_IP, PORT, USERNAME, XPC_MODE, DURATION)
     except KeyboardInterrupt:
         stop_event.set()
 
--- oran_dos/s_plane_ptp_dos_attack.py
+++ oran_dos/s_plane_ptp_dos_attack.py
@@ -13,8 +13,11 @@ from headers import *
 CHASSIS_IP = "demo.xenanetworks.com"
 USERNAME = "oran_dos"
 PORT = "7/0"
+XPC_MODE = True
+DURATION = 10
+XPC_FILENAME = "ptp_dos.xpc"
 
-async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
+async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str, xpc_mode: bool, duration: int):
 
     _mid = int(port_str.split("/")[0])
     _pid = int(port_str.split("/")[1])
@@ -24,11 +27,14 @@ async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
     print(f"Chassis:            {chassis_ip}")
     print(f"Username:           {username}")
     print(f"Port:               {port_str}")
+    print(f"XPC mode:           {xpc_mode}")
+    print(f"Test Duration:      {duration} sec")
     print(f"#####################################################################")
 
 
     # Establish connection to a Valkyrie tester using Python context manager
     # The connection will be automatically terminated when it is out of the block
+    print(f"Connect to chassis")
     async with testers.L23Tester(host=CHASSIS_IP, username=USERNAME, password="xena", port=22606, enable_logging=False) as tester:
 
         # Access module index 0 on the tester
@@ -41,63 +47,79 @@ async def s_plane_ptp_dos(chassis_ip: str, port_str: str, username: str):
         port = module.ports.obtain(_pid)
 
         # Forcibly reserve the TX port and reset it.
+        print(f"Reserve port {port.kind.module_id}/{port.kind.port_id}")
         await mgmt.reserve_port(port)
         await mgmt.reset_port(port)
         await asyncio.sleep(2)
 
-        # Configure port from .xpc file
-        # await cli.port_config_from_file(port, "ptp_dos.xpc")
+        if xpc_mode:
+            # Configure port from .xpc file
+            print(f"Load {XPC_FILENAME}")
+            await cli.port_config_from_file(port, XPC_FILENAME)
+        else:
+            print(f"Configure port and stream")
+            # Configure port using native python
+            await port.max_header_length.set(max_header_length=256) # Port's max header length = 128
+            stream = await port.streams.create() # create a stream on the port
+            
+            # Prepare packet header data
+            eth = Ethernet()
+            eth.dst_mac = "0100.5e00.0181"
+            eth.src_mac = "0030.051d.1e27"
+            eth.ethertype = "0800"
 
-        # Configure port using native python
-        await port.max_header_length.set(max_header_length=256) # Port's max header length = 128
-        stream = await port.streams.create() # create a stream on the port
-        
-        # Prepare packet header data
-        eth = Ethernet()
-        eth.dst_mac = "0100.5e00.0181"
-        eth.src_mac = "0030.051d.1e27"
-        eth.ethertype = "0800"
+            ipv4 = IPV4()
+            ipv4.src = "10.10.100.5"
+            ipv4.dst = "224.0.1.129"
+            ipv4.proto = 17
 
-        ipv4 = IPV4()
-        ipv4.src = "10.10.100.5"
-        ipv4.dst = "224.0.1.129"
-        ipv4.proto = 17
+            udp = UDP()
+            udp.src_port = 319
+            udp.dst_port = 319
+            udp.length = 132
 
-        udp = UDP()
-        udp.src_port = 319
-        udp.dst_port = 319
-        udp.length = 132
+            ptp = PTP()
+            
+            await utils.apply(
+                stream.comment.set(comment="S-Plane PTP DoS Attack"), # stream description
+                stream.rate.fraction.set(stream_rate_ppm=100_000), # rate in ppm, 10%=100_000, 100%=1_000_000
+                stream.burst.burstiness.set(size=0, density=100), # non-burst, burst size = 0, burst density = 100%
+                stream.packet.header.protocol.set(segments=[
+                    enums.ProtocolOption.ETHERNET, 
+                    enums.ProtocolOption.IP,
+                    enums.ProtocolOption.UDP,
+                    enums.ProtocolOption.RAW_124]), # stream header, Ethernet | IPv4 | UDP | RAW86
+                stream.packet.header.data.set(hex_data=Hex(str(eth)+str(ipv4)+str(udp)+str(ptp))),
+                stream.packet.length.set(length_type=enums.LengthType.FIXED, min_val=170, max_val=170),
+                stream.payload.content.set(payload_type=enums.PayloadType.INC8, hex_data=Hex("00")),
+                stream.tpld_id.set(test_payload_identifier=-1),
+                stream.insert_packets_checksum.set_on(),
+                stream.enable.set_on()
+            )
+            
+            await stream.packet.header.modifiers.configure(1) # create one modifier and configure
+            mod0 = stream.packet.header.modifiers.obtain(0) # access the created modifier
+            # configure the modifier
+            # place the modifier on header position 6
+            await mod0.specification.set(position=6, mask=Hex("FFFF0000"), action=enums.ModifierAction.RANDOM, repetition=1) 
+            await mod0.range.set(min_val=0, step=1, max_val=65535)
 
-        ptp = PTP()
-        
-        await utils.apply(
-            stream.comment.set(comment="S-Plane PTP DoS Attack"), # stream description
-            stream.rate.fraction.set(stream_rate_ppm=100_000), # rate in ppm, 10%=100_000, 100%=1_000_000
-            stream.burst.burstiness.set(size=0, density=100), # non-burst, burst size = 0, burst density = 100%
-            stream.packet.header.protocol.set(segments=[
-                enums.ProtocolOption.ETHERNET, 
-                enums.ProtocolOption.IP,
-                enums.ProtocolOption.UDP,
-                enums.ProtocolOption.RAW_124]), # stream header, Ethernet | IPv4 | UDP | RAW86
-            stream.packet.header.data.set(hex_data=Hex(str(eth)+str(ipv4)+str(udp)+str(ptp))),
-            stream.packet.length.set(length_type=enums.LengthType.FIXED, min_val=170, max_val=170),
-            stream.payload.content.set(payload_type=enums.PayloadType.INC8, hex_data=Hex("00")),
-            stream.tpld_id.set(test_payload_identifier=-1),
-            stream.insert_packets_checksum.set_on(),
-            stream.enable.set_on()
-        )
-        
-        await stream.packet.header.modifiers.configure(1) # create one modifier and configure
-        mod0 = stream.packet.header.modifiers.obtain(0) # access the created modifier
-        # configure the modifier
-        # place the modifier on header position 6
-        await mod0.specification.set(position=6, mask=Hex("FFFF0000"), action=enums.ModifierAction.RANDOM, repetition=1) 
-        await mod0.range.set(min_val=0, step=1, max_val=65535)
+        # start traffic
+        print(f"Start traffic")
+        await port.traffic.state.set_start()
+        # test duration
+        await asyncio.sleep(duration)
+        # stop traffic
+        print(f"Stop traffic")
+        await port.traffic.state.set_stop()
+        await mgmt.free_port(port)
+
+        print(f"Done")
 
 async def main():
     stop_event = asyncio.Event()
     try:
-        await s_plane_ptp_dos(CHASSIS_IP, PORT, USERNAME)
+        await s_plane_ptp_dos(CHASSIS_IP, PORT, USERNAME, XPC_MODE, DURATION)
     except KeyboardInterrupt:
         stop_event.set()
 
