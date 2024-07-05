c346c5e69d97583329b913988a724db3013c0fb0
Leonard Yu <leonard.yu@teledyne.com>
2024-07-05 11:57:33 +0200

    Update L1 bit rate

--- l1_bit_rate/l1_bit_rate.py
+++ l1_bit_rate/l1_bit_rate.py
@@ -1,3 +1,28 @@
+################################################################
+#
+#                   L1 BIT RATE
+#
+# What this script example does:
+# 1. Connect to a tester
+# 2. Reserve a port for TX and another one for RX
+# 3. Create a simple stream on TX port with 100% rate
+# 4. Start traffic on TX port
+# 5. Read RX and TX L2 traffic statistics
+# 6. Calculate L1 traffic rate based on L2 statistics
+# 7. Stop traffic
+# 
+# As to traffic rate statistics, you can only get L2 statistics 
+# from the commands, including bits per second (bps) and frames per second (fps). 
+# Use the following equation to calculate L1 bits per second.
+# 
+# * On TX side: ``l1_bit_per_sec = l2_fps * (interframe_gap + frame_size) * 8``
+# * On RX side: ``l1_bit_per_sec = l2_fps * interframe_gap * 8 + l2_bit_per_sec``
+# 
+# > Note: The **inter-frame gap on both TX and RX sides must be the same**, 
+# otherwise the calculated L1 bps values will be different.
+#
+################################################################
+
 import asyncio
 
 from xoa_driver import testers
@@ -8,23 +33,42 @@ from xoa_driver import utils
 from xoa_driver.hlfuncs import mgmt
 from xoa_driver.misc import Hex
 import ipaddress
+import logging
 
+#---------------------------
+# GLOBAL PARAMS
+#---------------------------
 CHASSIS_IP = "demo.xenanetworks.com"
-USERNAME = "l1_bit_rate"
-TX_MODULE_IDX = 2
-TX_PORT_IDX = 4
-RX_MODULE_IDX = 3
-RX_PORT_IDX = 0
+USERNAME = "XOA"
+TX_PORT = "0/0"
+RX_PORT = "0/1"
 
-async def my_awesome_func(stop_event: asyncio.Event):
+async def l1_bit_rate(chassis: str, username: str, tx_port_str: str, rx_port_str: str):
+
+    # configure basic logger
+    logging.basicConfig(
+        format="%(asctime)s  %(message)s",
+        level=logging.DEBUG,
+        handlers=[
+            logging.FileHandler(filename="test.log", mode="a"),
+            logging.StreamHandler()]
+        )
 
     # Establish connection to a Valkyrie tester using Python context manager
     # The connection will be automatically terminated when it is out of the block
-    async with testers.L23Tester(host=CHASSIS_IP, username=USERNAME, password="xena", port=22606, enable_logging=False) as tester:
+    async with testers.L23Tester(host=chassis, username=username, password="xena", port=22606, enable_logging=False) as tester:
+
+        logging.info(f"===================================")
+        logging.info(f"{'Connect to chassis:':<20}{chassis}")
+        logging.info(f"{'Username:':<20}{username}")
 
         # Access module index 0 on the tester
-        tx_module = tester.modules.obtain(TX_MODULE_IDX)
-        rx_module = tester.modules.obtain(RX_MODULE_IDX)
+        _mid_tx = int(tx_port_str.split("/")[0])
+        _pid_tx = int(tx_port_str.split("/")[1])
+        _mid_rx = int(rx_port_str.split("/")[0])
+        _pid_rx = int(rx_port_str.split("/")[1])
+        tx_module = tester.modules.obtain(_mid_tx)
+        rx_module = tester.modules.obtain(_mid_rx)
 
         if isinstance(tx_module, modules.ModuleChimera):
             return None
@@ -32,8 +76,8 @@ async def my_awesome_func(stop_event: asyncio.Event):
             return None
 
         # Get the port object on module
-        tx_port = tx_module.ports.obtain(TX_PORT_IDX)
-        rx_port = rx_module.ports.obtain(RX_PORT_IDX)
+        tx_port = tx_module.ports.obtain(_pid_tx)
+        rx_port = rx_module.ports.obtain(_pid_rx)
 
         await mgmt.reserve_port(tx_port)
         await mgmt.reset_port(tx_port)
@@ -53,7 +97,7 @@ async def my_awesome_func(stop_event: asyncio.Event):
         # Stream index is automatically assigned
         stream = await tx_port.streams.create()
         stream_index = stream.idx
-        print(f"TX stream index: {stream_index}")
+        logging.info(f"TX stream index: {stream_index}")
 
         # Simple batch configure the stream on the TX port
         await utils.apply(
@@ -88,15 +132,15 @@ async def my_awesome_func(stop_event: asyncio.Event):
 
         resp = await tx_port.interframe_gap.get()
         tx_ifg = resp.min_byte_count
-        print(f"TX port inter-frame gap: {tx_ifg} bytes")
+        logging.info(f"TX port inter-frame gap: {tx_ifg} bytes")
 
         resp = await stream.packet.length.get()
         tx_frame_size = resp.min_val
-        print(f"TX port frame size: {tx_frame_size} bytes")
+        logging.info(f"TX port frame size: {tx_frame_size} bytes")
 
         resp = await rx_port.interframe_gap.get()
         rx_ifg = resp.min_byte_count
-        print(f"RX port inter-frame gap: {rx_ifg} bytes")
+        logging.info(f"RX port inter-frame gap: {rx_ifg} bytes")
 
         for i in range(10):
             # Query port-level traffic statistics
@@ -110,10 +154,10 @@ async def my_awesome_func(stop_event: asyncio.Event):
             tx_fps = resp1.packet_count_last_sec
             tx_bps_l1 = tx_fps*(tx_ifg+tx_frame_size)*8
 
-            print("*"*(i+1))
-            print(f"TX bits per second (L1): {tx_bps_l1} bps")
-            print(f"TX bits per second (L2): {tx_bps_l2} bps")
-            print(f"TX frames per second: {tx_fps} fps")
+            logging.info("*"*(i+1))
+            logging.info(f"TX bits per second (L1): {tx_bps_l1} bps")
+            logging.info(f"TX bits per second (L2): {tx_bps_l2} bps")
+            logging.info(f"TX frames per second: {tx_fps} fps")
 
             # resp = await rx_port.statistics.rx.total.get()
             
@@ -121,9 +165,9 @@ async def my_awesome_func(stop_event: asyncio.Event):
             rx_fps = resp2.packet_count_last_sec
             rx_bps_l1 = rx_fps*rx_ifg*8 + rx_bps_l2
 
-            print(f"RX bits per second (L1): {rx_bps_l1} bps")
-            print(f"RX bits per second (L2): {rx_bps_l2} bps")
-            print(f"RX frames per second: {rx_fps} fps")
+            logging.info(f"RX bits per second (L1): {rx_bps_l1} bps")
+            logging.info(f"RX bits per second (L2): {rx_bps_l2} bps")
+            logging.info(f"RX frames per second: {rx_fps} fps")
 
             await asyncio.sleep(1)
 
@@ -140,7 +184,12 @@ async def my_awesome_func(stop_event: asyncio.Event):
 async def main():
     stop_event = asyncio.Event()
     try:
-        await my_awesome_func(stop_event)
+        await l1_bit_rate(
+            chassis=CHASSIS_IP,
+            username=USERNAME,
+            tx_port_str=TX_PORT,
+            rx_port_str=RX_PORT
+        )
     except KeyboardInterrupt:
         stop_event.set()
 
