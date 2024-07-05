befb4954445c2627380b9179e9d1a4e88cee4934
Leonard Yu <leonard.yu@teledyne.com>
2024-07-05 14:15:44 +0200

    Update PRBS stats

--- prbs_ber_stats/prbs_ber_stats.py
+++ prbs_ber_stats/prbs_ber_stats.py
@@ -1,3 +1,16 @@
+################################################################
+#
+#                   PRBS BER STATISTICS
+#
+# What this script example does:
+# 1. Connect to a tester
+# 2. Reserve a port. Must be Freya, Thor, or Loki
+# 3. Reset the port
+# 4. Set the port PRBS mode on
+# 5. Collect per-second statistics
+#
+################################################################
+
 import asyncio
 
 from xoa_driver import testers
@@ -10,53 +23,125 @@ from xoa_driver.misc import Hex
 import ipaddress
 import csv
 import time
+import logging
 
-CHASSIS_IP = "10.20.1.166"
-USERNAME = "xoa"
-MODULE_IDX = 4
-PORT_IDX = 0
-_FREYA_THOR_LOKI_MODULES = (modules.MFreya800G4S1P_a, modules.MFreya800G4S1P_b, modules.MFreya800G4S1POSFP_a, modules.MFreya800G4S1POSFP_b, modules.MThor400G7S1P, modules.MThor400G7S1P_b, modules.MThor400G7S1P_c, modules.MThor400G7S1P_d, modules.MLoki100G5S1P, modules.MLoki100G5S2P)
-        
+#---------------------------
+# GLOBAL PARAMS
+#---------------------------
+CHASSIS_IP = "10.165.136.70"
+USERNAME = "XOA"
+PORT1 = "4/0"       
+PORT2 = "4/1"
+DURATION = 20
 
-async def my_awesome_func(stop_event: asyncio.Event):
+#---------------------------
+# prbs_ber_stats
+#---------------------------
+async def prbs_ber_stats(chassis: str, username: str, port_str1: str, port_str2: str, duration: int):
+    # configure basic logger
+    logging.basicConfig(
+        format="%(asctime)s  %(message)s",
+        level=logging.DEBUG,
+        handlers=[
+            logging.FileHandler(filename="test.log", mode="a"),
+            logging.StreamHandler()]
+        )
 
     # Establish connection to a Valkyrie tester using Python context manager
     # The connection will be automatically terminated when it is out of the block
-    async with testers.L23Tester(host=CHASSIS_IP, username=USERNAME, password="xena", port=22606, enable_logging=False) as tester:
+    async with testers.L23Tester(host=chassis, username=username, password="xena", port=22606, enable_logging=False) as tester:
         
-        # Access module index 0 on the tester
-        module = tester.modules.obtain(MODULE_IDX)
+        # Access module on the tester
+        _mid1 = int(port_str1.split("/")[0])
+        _pid1 = int(port_str1.split("/")[1])
+        _mid2 = int(port_str2.split("/")[0])
+        _pid2 = int(port_str2.split("/")[1])
+        module_obj1 = tester.modules.obtain(_mid1)
+        module_obj2 = tester.modules.obtain(_mid2)
 
-        if not isinstance(module, _FREYA_THOR_LOKI_MODULES):
-            print(f"Not Freya or Thor or Loki module")
+        if isinstance(module_obj1, modules.Z10OdinModule):
+            logging.info(f"Not Freya or Thor or Loki module")
+            return None
+        if isinstance(module_obj2, modules.Z10OdinModule):
+            logging.info(f"Not Freya or Thor or Loki module")
             return None 
 
-        # Get the port 0 on module 0 as TX port
-        port = module.ports.obtain(PORT_IDX)
+        # Get the port as TX port
+        port_obj1 = module_obj1.ports.obtain(_pid1)
+        port_obj2 = module_obj2.ports.obtain(_pid2)
 
         # Forcibly reserve the port and reset it.
-        await mgmt.reserve_port(port)
-        await mgmt.reset_port(port)
+        await mgmt.reserve_port(port_obj1)
+        await mgmt.reset_port(port_obj1)
+        await mgmt.reserve_port(port_obj2)
+        await mgmt.reset_port(port_obj2)
 
         await asyncio.sleep(5)
 
         # Check how many serdes are there
-        _p_capability = await port.capabilities.get()
-        _serdes_count = _p_capability.serdes_count
-        print(f"Port {MODULE_IDX}/{PORT_IDX} Serdes Count: {_serdes_count}")
+        _p_capability1 = await port_obj1.capabilities.get()
+        _serdes_count1 = _p_capability1.serdes_count
+        logging.info(f"Port {_mid1}/{_pid1} Serdes Count: {_serdes_count1}")
+        _p_capability2 = await port_obj2.capabilities.get()
+        _serdes_count2 = _p_capability2.serdes_count
+        logging.info(f"Port {_mid2}/{_pid2} Serdes Count: {_serdes_count2}")
 
         # Set PRBS TX Config
-        await port.pcs_pma.prbs_config.tx_type.set(
+        await port_obj1.pcs_pma.prbs_config.type.set(
             prbs_inserted_type=enums.PRBSInsertedType.PHY_LINE,
-            prbs_pattern=enums.PRBSPattern.PRBS31,
-            invert=enums.PRBSInvertState.NON_INVERTED)
+            polynomial=enums.PRBSPolynomial.PRBS31,
+            invert=enums.PRBSInvertState.NON_INVERTED,
+            statistics_mode=enums.PRBSStatisticsMode.PERSECOND)
+        await port_obj2.pcs_pma.prbs_config.type.set(
+            prbs_inserted_type=enums.PRBSInsertedType.PHY_LINE,
+            polynomial=enums.PRBSPolynomial.PRBS31,
+            invert=enums.PRBSInvertState.NON_INVERTED,
+            statistics_mode=enums.PRBSStatisticsMode.PERSECOND)
 
-        await port.serdes[0].prbs.tx_config.set(prbs_seed=0, prbs_on_off=enums.PRBSOnOff.PRBSON)
+        # Enable PRBS on all serdes
+        for i in range(_serdes_count1):
+            await port_obj1.serdes[i].prbs.tx_config.set(prbs_seed=0, prbs_on_off=enums.PRBSOnOff.PRBSON, error_on_off=enums.ErrorOnOff.ERRORSOFF)
+
+        _list = []
+        for i in range(_serdes_count1):
+            _list.append(port_obj1.serdes[i].prbs.tx_config.set(prbs_seed=0, prbs_on_off=enums.PRBSOnOff.PRBSON, error_on_off=enums.ErrorOnOff.ERRORSOFF))
+        await utils.apply(*_list)
+
+        await asyncio.sleep(2.0)
+
+        # clear counters
+        await port_obj2.pcs_pma.rx.clear.set()
+
+        # Sample PRBS status counter on the other port every second for 20 secs.
+        _count = 0
+        _list = []
+        for i in range(_serdes_count1):
+            _list.append(port_obj2.serdes[i].prbs.status.get())
+        while _count <= duration-1:
+            resp = await utils.apply(*_list)
+            print(f"*"*_count)
+            for i in range(_serdes_count2):
+                if resp[i].error_count > 0:
+                    print(f"Serdes {i}, PRBS Lock={resp[i].lock.name}, PRBS Bits={resp[i].byte_count*8}, PRBS Errors={resp[i].error_count}, Error Rate={resp[i].error_count/resp[0].byte_count/8}")
+                else:
+                    print(f"Serdes {i}, PRBS Lock={resp[i].lock.name}, PRBS Bits={resp[i].byte_count*8}, PRBS Errors={resp[i].error_count}, Error Rate<{4.6/resp[0].byte_count/8}")
+            await asyncio.sleep(1.0)
+            _count += 1
+
+        # Stop PRBS on the tx port all serdes
+        _list = []
+        for i in range(_serdes_count1):
+            _list.append(port_obj1.serdes[i].prbs.tx_config.set(prbs_seed=0, prbs_on_off=enums.PRBSOnOff.PRBSOFF, error_on_off=enums.ErrorOnOff.ERRORSOFF))
+        await utils.apply(*_list)
+
+        # Release the ports
+        await mgmt.free_port(port=port_obj1)
+        await mgmt.free_port(port=port_obj2)
 
 async def main():
     stop_event = asyncio.Event()
     try:
-        await my_awesome_func(stop_event)
+        await prbs_ber_stats(chassis=CHASSIS_IP, username=USERNAME, port_str1=PORT1, port_str2=PORT2, duration=DURATION)
     except KeyboardInterrupt:
         stop_event.set()
 
