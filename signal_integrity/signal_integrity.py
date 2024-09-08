edc02bd0b535f015bce64c204f3c0eb09c5df895
Leonard Yu <leonard.yu@teledyne.com>
2024-09-08 12:01:10 +0200

    Add slicers and allow selection of lanes

--- signal_integrity/signal_integrity.py
+++ signal_integrity/signal_integrity.py
@@ -20,6 +20,8 @@ from xoa_driver import utils
 from xoa_driver.hlfuncs import mgmt
 from xoa_driver.misc import Hex
 import logging
+import math
+from typing import List
 
 import matplotlib.pyplot as plt
 from collections import deque
@@ -30,15 +32,22 @@ from collections import deque
 CHASSIS_IP = "10.165.136.60"
 USERNAME = "xoa"
 PORT = "3/0"
-FIGURE_TITLE = "Signal Integrity Histogram View"
+FIGURE_TITLE = "Z800 Freya Signal Integrity Histogram Plot"
 DENSITY = 1 # how many batches of siv data to show on the plot. A higher density means more data and slower plotting.
+LANES = [0,1,2,3,4,5,6,7] # select lanes to display, ranging from 0 to 7
+PLOTTING_INTERVAL = 1 # plots refreshed every n second
+PLOTTING_DURATION = 120 # number of seconds for plotting
 
 async def siv_plot(
         chassis: str, 
         username: str, 
         port_str: str,
         figure_title: str,
-        density: int):
+        density: int,
+        plotting_interval: int,
+        plotting_duration: int,
+        lanes: List[int],
+        ):
     
     # configure basic logger
     logging.basicConfig(
@@ -49,11 +58,24 @@ async def siv_plot(
             logging.StreamHandler()]
         )
     
+    # disable matplotlib.font_manager logging
+    logging.getLogger('matplotlib.font_manager').disabled = True
+
+    # remove duplicates and sort list
+    lanes = list(set(lanes))
+    lanes.sort()
+    
     async with testers.L23Tester(host=chassis, username=username, password="xena", port=22606, enable_logging=False) as tester:
         logging.info(f"#####################################################################")
         logging.info(f"Chassis:                 {chassis}")
         logging.info(f"Username:                {username}")
         logging.info(f"Port:                    {port_str}")
+        logging.info(f"Lanes:                   {lanes}")
+        logging.info(f"Figure Title:            {figure_title}")
+        logging.info(f" Data Density:            {density}")
+        logging.info(f" Plot Refresh Interval:   {plotting_interval} s")
+        logging.info(f" Plot Duration:           {plotting_duration} s")
+        logging.info(f"#####################################################################")
 
         # Access module on the tester
         _mid = int(port_str.split("/")[0])
@@ -68,160 +90,100 @@ async def siv_plot(
         await mgmt.free_module(module=module_obj, should_free_ports=False)
         await mgmt.reserve_port(port_obj)
 
+        resp = await port_obj.capabilities.get()
+        max_serdes = resp.serdes_count
+        serdes_cnt_to_show = len(lanes)
+        if max(lanes) > 7:
+            logging.warning(f"Exceed max serdes index. Abort.")
+            return None
+        if serdes_cnt_to_show > max_serdes:
+            logging.warning(f"Exceed max serdes count. Abort.")
+            return None
+        if serdes_cnt_to_show == 0:
+            logging.warning(f"Nothing to show Abort.")
+            return None
+
         # figure config
         plt.ion()
         fig = plt.figure(constrained_layout=True)
-        fig.suptitle(f"{figure_title} d={density}")
-        gs = fig.add_gridspec(nrows=4, ncols=2)
-        siv0 = fig.add_subplot(gs[0, 0])  # siv for lane 0
-        siv1 = fig.add_subplot(gs[1, 0])  # siv for lane 1
-        siv2 = fig.add_subplot(gs[2, 0])  # siv for lane 2
-        siv3 = fig.add_subplot(gs[3, 0])  # siv for lane 3
-        siv4 = fig.add_subplot(gs[0, 1])  # siv for lane 4
-        siv5 = fig.add_subplot(gs[1, 1])  # siv for lane 5
-        siv6 = fig.add_subplot(gs[2, 1])  # siv for lane 6
-        siv7 = fig.add_subplot(gs[3, 1])  # siv for lane 7
+        fig.suptitle(f"{figure_title} D={density}, L={lanes}")
 
-        h0 = ()
-        h1 = ()
-        h2 = ()
-        h3 = ()
-        h4 = ()
-        h5 = ()
-        h6 = ()
-        h7 = ()
+        # grid spec: 4 rows and 2 columns
+        if serdes_cnt_to_show == 1:
+            gs = fig.add_gridspec(nrows=1, ncols=1)
+        if serdes_cnt_to_show > 1:
+            gs = fig.add_gridspec(nrows=math.ceil(serdes_cnt_to_show/2), ncols=2)
+
+        # add subplots
+        siv_subplots = []
+        for i in range(serdes_cnt_to_show):
+            siv_subplots.append(fig.add_subplot(gs[i%gs.nrows, int(i/gs.nrows)]))
         
-        DATA_PER_BATCH = 2000
-        data0 = deque(h0, maxlen=density*DATA_PER_BATCH)
-        data1 = deque(h1, maxlen=density*DATA_PER_BATCH)
-        data2 = deque(h2, maxlen=density*DATA_PER_BATCH)
-        data3 = deque(h3, maxlen=density*DATA_PER_BATCH)
-        data4 = deque(h4, maxlen=density*DATA_PER_BATCH)
-        data5 = deque(h5, maxlen=density*DATA_PER_BATCH)
-        data6 = deque(h6, maxlen=density*DATA_PER_BATCH)
-        data7 = deque(h7, maxlen=density*DATA_PER_BATCH)
+        # data dequeue for each serdes lane. queue depth = density*2000
+        INT_CNT_PER_DATA = 2000
+        data_queue = []
+        for _ in range(serdes_cnt_to_show):
+            data_queue.append(deque((), maxlen=density*INT_CNT_PER_DATA))
 
-        siv0.set(xlabel=f"Value", ylabel=f"Lane 0")
-        siv1.set(xlabel=f"Value", ylabel=f"Lane 1")
-        siv2.set(xlabel=f"Value", ylabel=f"Lane 2")
-        siv3.set(xlabel=f"Value", ylabel=f"Lane 3")
-        siv4.set(xlabel=f"Value", ylabel=f"Lane 4")
-        siv5.set(xlabel=f"Value", ylabel=f"Lane 5")
-        siv6.set(xlabel=f"Value", ylabel=f"Lane 6")
-        siv7.set(xlabel=f"Value", ylabel=f"Lane 7")
+        # set x and y label for each subplot
+        for i in range(serdes_cnt_to_show):
+            siv_subplots[i].set(xlabel=f"Value", ylabel=f"Lane {lanes[i]}")
+        
+        # group control commands for each serdes lane together to later send it as a command group.
+        control_cmd_group = []
+        for i in range(serdes_cnt_to_show):
+            control_cmd_group.append(port_obj.l1.serdes[lanes[i]].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN))
+        
+        # get commands for each serdes lane together to later send it as a command group.
+        get_cmd_group = []
+        for i in range(serdes_cnt_to_show):
+            get_cmd_group.append(port_obj.l1.serdes[lanes[i]].medium.siv.data.get())
 
-        n = 0
-        while True:
-            print(f"control {n}")
-            await utils.apply(
-                port_obj.l1.serdes[0].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[1].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[2].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[3].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[4].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[5].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[6].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-                port_obj.l1.serdes[7].medium.siv.control.set(opcode=enums.Layer1Opcode.START_SCAN),
-            )
+        resp_group = ()
+        plot_count = math.ceil(plotting_duration/plotting_interval)
+        for _ in range(plot_count):
+            await utils.apply(*control_cmd_group)
             while True:
-                resp0, resp1, resp2, resp3, resp4, resp5, resp6, resp7 = await utils.apply(
-                    port_obj.l1.serdes[0].medium.siv.data.get(),
-                    port_obj.l1.serdes[1].medium.siv.data.get(),
-                    port_obj.l1.serdes[2].medium.siv.data.get(),
-                    port_obj.l1.serdes[3].medium.siv.data.get(),
-                    port_obj.l1.serdes[4].medium.siv.data.get(),
-                    port_obj.l1.serdes[5].medium.siv.data.get(),
-                    port_obj.l1.serdes[6].medium.siv.data.get(),
-                    port_obj.l1.serdes[7].medium.siv.data.get(),
-                )
-                if resp0.result == 0 or resp1.result == 0 or resp2.result == 0 or resp3.result == 0 or resp4.result == 0 or resp5.result == 0 or resp6.result == 0 or resp7.result == 0:
+                # get responses from all lanes
+                resp_group = await utils.apply(*get_cmd_group)
+                if 0 in resp_group:
+                    # if not all lanes are ready in data, query again.
                     continue
                 else:
-                    lst0 = resp0.value[12:]
-                    tmp0 = []
-                    for x in zip(lst0[0::2], lst0[1::2]):
-                        tmp0.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data0.extend(tuple(tmp0))
-                    siv0.relim()
-                    siv0.autoscale_view()
-                    print(f"data0 len = {len(data0)}")
-                    siv0.hist(x=[*data0], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
+                    for i in range(serdes_cnt_to_show):
+                        siv_raw_levels = resp_group[i].value[0:12]
+                        siv_raw_values = resp_group[i].value[12:]
 
-                    lst1 = resp1.value[12:]
-                    tmp1 = []
-                    for x in zip(lst1[0::2], lst1[1::2]):
-                        tmp1.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data1.extend(tuple(tmp1))
-                    siv1.relim()
-                    siv1.autoscale_view()
-                    print(f"data1 len = {len(data1)}")
-                    siv1.hist(x=[*data1], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
+                        # convert from 12 raw bytes into 6 signed int
+                        siv_int_levels = []
+                        for x in zip(siv_raw_levels[0::2], siv_raw_levels[1::2]):
+                            siv_int_levels.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
+                        # Please note: only the first slicer data is used here.
 
-                    lst2 = resp2.value[12:]
-                    tmp2 = []
-                    for x in zip(lst2[0::2], lst2[1::2]):
-                        tmp2.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data2.extend(tuple(tmp2))
-                    siv2.relim()
-                    siv2.autoscale_view()
-                    print(f"data2 len = {len(data2)}")
-                    siv2.hist(x=[*data2], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
+                        # convert from 4000 bytes into 2000 signed int
+                        siv_int_values = []
+                        for x in zip(siv_raw_values[0::2], siv_raw_values[1::2]):
+                            siv_int_values.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
+                        # put value data in queue
+                        data_queue[i].extend(tuple(siv_int_values))
 
-                    lst3 = resp3.value[12:]
-                    tmp3 = []
-                    for x in zip(lst3[0::2], lst3[1::2]):
-                        tmp3.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data3.extend(tuple(tmp3))
-                    siv3.relim()
-                    siv3.autoscale_view()
-                    print(f"data3 len = {len(data3)}")
-                    siv3.hist(x=[*data3], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
-
-                    lst4 = resp4.value[12:]
-                    tmp4 = []
-                    for x in zip(lst4[0::2], lst4[1::2]):
-                        tmp4.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data4.extend(tuple(tmp4))
-                    siv4.relim()
-                    siv4.autoscale_view()
-                    print(f"data4 len = {len(data4)}")
-                    siv4.hist(x=[*data4], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
-
-                    lst5 = resp5.value[12:]
-                    tmp5 = []
-                    for x in zip(lst5[0::2], lst5[1::2]):
-                        tmp5.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data5.extend(tuple(tmp5))
-                    siv5.relim()
-                    siv5.autoscale_view()
-                    print(f"data5 len = {len(data5)}")
-                    siv5.hist(x=[*data5], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
-
-                    lst6 = resp6.value[12:]
-                    tmp6 = []
-                    for x in zip(lst6[0::2], lst6[1::2]):
-                        tmp6.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data6.extend(tuple(tmp6))
-                    siv6.relim()
-                    siv6.autoscale_view()
-                    print(f"data6 len = {len(data6)}")
-                    siv6.hist(x=[*data6], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
-
-                    lst7 = resp7.value[12:]
-                    tmp7 = []
-                    for x in zip(lst7[0::2], lst7[1::2]):
-                        tmp7.append(int.from_bytes(bytes(x), byteorder='big', signed=True))
-                    data7.extend(tuple(tmp7))
-                    siv7.relim()
-                    siv7.autoscale_view()
-                    print(f"data7 len = {len(data7)}")
-                    siv7.hist(x=[*data7], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
+                        # siv data ranges from -64 to 63, thus 128 bins in total.
+                        siv_subplots[i].relim()
+                        siv_subplots[i].autoscale_view()
+                        siv_subplots[i].hist(x=[*data_queue[i]], bins=128, range=(-64, 63), density=False, color="blue", orientation="horizontal")
+                        # add base slicer
+                        siv_subplots[i].axhline(0, color='black', linestyle='dashed', linewidth=0.1)
+                        # add upper slicer
+                        siv_subplots[i].axhline(siv_int_levels[1], color='black', linestyle='dashed', linewidth=0.1)
+                        # add lower slicer
+                        siv_subplots[i].axhline(siv_int_levels[4], color='black', linestyle='dashed', linewidth=0.1)
 
                     plt.show()
-                    plt.pause(1)
-                    print(f"plot {n}")
-                    n += 1
+                    plt.pause(plotting_interval)
                     break
+        
+        await mgmt.free_port(port_obj)
+        logging.info(f"Bye!")
 
 async def main():
     stop_event = asyncio.Event()
@@ -231,7 +193,10 @@ async def main():
             username=USERNAME,
             port_str=PORT,
             figure_title=FIGURE_TITLE,
-            density=DENSITY
+            density=DENSITY,
+            plotting_interval=PLOTTING_INTERVAL,
+            plotting_duration = PLOTTING_DURATION,
+            lanes=LANES
             )
     except KeyboardInterrupt:
         stop_event.set()
